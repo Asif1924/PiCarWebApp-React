87d90986332687e2a6787723ee6965f2b2b86b52
Asif Alli <alli.asif@gmail.com>
2020-07-07 11:34:17 -0400

    added stuff

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var carStream = 'http://192.168.1.183:9787/?action=stream';
   return (
-      <div className="PageContents">
-
+      <div className="ScreenLayoutFullWidth">
+        <div className="controllerImage" ></div>
       </div>
   );
 }
