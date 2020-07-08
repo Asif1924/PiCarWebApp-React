c880e7e38548ec5573b8712100f4d56b7460ae99
Asif Alli <alli.asif@gmail.com>
2020-07-07 23:26:14 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var carStream = 'http://192.168.1.183:9787/?action=stream';
+
+  const imageURL = "http://192.168.1.183:9787/?action=stream";
+
+  const imageURLStyle = {
+    backgroundImage: "url(" + imageURL + ")"
+  };
+
+  //-background-image:url( http://192.168.1.183:9787/?action=stream ); 
+/*
+.statusArea{
+  z-index:5;
+  position:fixed; 
+  top: 5px; left: ${offset}; 
+  width:410px; 
+  height: 50px; 
+  border:none;
+  border-color:black
+} */
   return (
       <div className="ScreenLayoutFullWidth">
-        <div className="controllerImage" ></div>
+        <div className="controllerImage" style={imageURLStyle}></div>
       </div>
   );
 }
