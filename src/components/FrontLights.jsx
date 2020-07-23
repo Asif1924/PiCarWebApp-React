51326cb65e04b7c3d804c27525bd0126359d1bc7
Asif Alli <alli.asif@gmail.com>
2020-07-22 20:22:59 -0400

    much simpler code for lights

--- src/components/FrontLights.jsx
+++ src/components/FrontLights.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import lightsOffImage from '../images/FrontlightsOff.png';
-import lightsOnImage from '../images/FrontlightsOn.png';
+
 
 class FrontLights extends Component {
 
@@ -9,9 +8,6 @@ class FrontLights extends Component {
         this.state = {
                         lights:'off',
                         lightsImage:"images/FrontlightsOff.png",
-                        backgroundImage:"url(../images/FrontlightsOff.png)",
-                        imgSrcOn:"../images/FrontlightsOn.png",
-                        imgSrcOff:"../images/FrontlightsOff.png"
                         };
         this.handleClick=this.handleClick.bind(this);
     }
@@ -36,29 +32,17 @@ class FrontLights extends Component {
     }
 
     handleClick = (event) => {
-        //console.log("handleClick");  
-        //console.log("handleClick", event);  
-        //console.log(event);  
         if(this.state.lights==="on"){
             this.setState({lights:"off"});
             this.setState({lightsImage:"images/FrontlightsOff.png"});
-            this.setState({backgroundImage:"url(images/FrontlightsOff.png)"});
-            this.style={backgroundImage:`url(${lightsOffImage})`}
-            console.log(this.style);  
         }else{
             this.setState({lights:"on"});
             this.setState({lightsImage:"images/FrontlightsOn.png"});
-            this.setState({backgroundImage:"url(images/FrontlightsOn.png)"});
-            this.style={backgroundImage:`url(${lightsOnImage})`}
-            console.log(this.style);  
         }
-        
+        console.log(this.style);  
     }
 
     render() {
-        var imgURL = this.state.lights==="on" ? this.state.imgSrcOn : this.state.imgSrcOff;
-        //console.log( imgURL );
-
         var buttonStyle = {
             zIndex:'5',
             position:'fixed',
@@ -72,14 +56,11 @@ class FrontLights extends Component {
             backgroundSize:'cover',
             objectFit:'contain'                 
         }
-        //console.log( "divStyle = " + divStyle);
-        //console.log( divStyle );
         
         return (
-            //<div className="frontLightsButton" style={divStyle} onClick={this.handleClick}>
-            //<div style={this.getStyle()} onClick={this.handleClick.bind(this)}>            
             <div style={buttonStyle} onClick={this.handleClick}>
                 <img
+                    alt={this.state.lights}
                     style={{
                         width:'30px',
                         height:'30px',
